c4fd5e657e5a50ebb7cec1c4162945fa14f4db17
micazev <micneaz@gmail.com>
2023-12-18 07:53:43 -0300

    esta abrindo e pegando o número da nota, porém não volta á lista

--- navigation/click_each_nfse.py
+++ navigation/click_each_nfse.py
@@ -1,17 +1,13 @@
-# navigation/click_each_nfse.py
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import StaleElementReferenceException, NoSuchElementException, TimeoutException
 from bs4 import BeautifulSoup
 import logging
 import time
 
 def click_each_nfse(nav):
     try:
-        # Switch to the main window
-        main_window_handle = nav.window_handles[0]
-        nav.switch_to.window(main_window_handle)
-
         # Locate the table rows
         table_rows = nav.find_elements(By.XPATH, '//table[@border="0"]/tbody/tr[contains(@class, "gridResultado")]')
 
@@ -20,20 +16,41 @@ def click_each_nfse(nav):
         print(f"Number of rows to click: {row_count}")
         logging.info(f"Number of rows to click: {row_count}")
 
+        # Get the main window handle
+        main_window_handle = nav.current_window_handle
+
         # Iterate through each row and click on the first column item
         for row in table_rows:
             first_column_item = row.find_element(By.XPATH, './td[@align="left" and contains(@class, "right")]/a')
             first_column_item_text = first_column_item.text
             first_column_item.click()
-            time.sleep(50)
-            nota_number = extract_nota_number(nav, first_column_item_text)
+            print("estou aqui!")
+            time.sleep(5)
 
-            # Perform actions on the details page
-            # extract_nota_fiscal_details(nav, nota_number)
+            try:
+                # Wait for the new window to appear
+                WebDriverWait(nav, 10).until(
+                    lambda driver: len(driver.window_handles) > 1
+                )
 
-            # After processing the details page, close the current window and go back to the main table page
-            nav.close()
-            nav.switch_to.window(main_window_handle)
+                # Switch to the new window
+                new_window_handle = nav.window_handles[-1]
+                nav.switch_to.window(new_window_handle)
+
+                # Perform actions on the new window (extract nota number, etc.)
+                extract_nota_number(nav, first_column_item_text)
+
+            except (TimeoutException, StaleElementReferenceException, NoSuchElementException) as e:
+                logging.error(f"Error switching to new window: {e}")
+                # Handle any exception and continue with the next row
+
+            finally:
+                # Close the new window
+                if len(nav.window_handles) > 1:
+                    nav.close()
+
+                # Switch back to the main window
+                nav.switch_to.window(main_window_handle)
 
     except Exception as e:
         logging.error(f"Error in click_each_nfse: {e}")
@@ -51,10 +68,9 @@ def extract_nota_number(nav, item_text):
             nota_value = nota_element.text.strip()
             print(f"Numero da Nota: {nota_value}")
             logging.info(f"Numero da Nota: {nota_value}")
+
         else:
             logging.warning("Element with class 'impressaoTitulo' not found.")
-
-        return nota_value
 
     except Exception as e:
         logging.error(f"Error in extract_nota_fiscal_details: {e}")
